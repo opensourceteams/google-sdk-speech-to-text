e52c527672e2387f1e5ac2aa374c2699078eab7d
thinktothings <thinktothings@gmail.com>
2019-04-02 09:06:13 +0800

    ## 识别率超高的在线麦克风语音转文本(免费工具) - https://www.speechtexter.com

--- src/main/java/com/opensourceteams/module/google/speech/to/text/simple/asynLlong/RunCNPCM.java
+++ src/main/java/com/opensourceteams/module/google/speech/to/text/simple/asynLlong/RunCNPCM.java
@@ -2,30 +2,29 @@ package com.opensourceteams.module.google.speech.to.text.simple.asynLlong;
 
 
 import com.google.api.gax.longrunning.OperationFuture;
-import com.google.cloud.speech.v1.LongRunningRecognizeMetadata;
-import com.google.cloud.speech.v1.LongRunningRecognizeResponse;
-import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
-import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.cloud.speech.v1p1beta1.*;
 import com.google.protobuf.ByteString;
 import org.apache.log4j.Logger;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
  * - 美国英语 (en-US) 或英国英语 (en-GB)
  * - 普通话 (中国大陆)	cmn-Hans-CN	中文、普通话（中国简体）
  */
-public class RunCN {
+public class RunCNPCM {
 
-    static Logger logger = Logger.getLogger(RunCN.class);
+    static Logger logger = Logger.getLogger(RunCNPCM.class);
     public static void main(String[] args) throws Exception {
         logger.info("开始");
         //限制10M
-        transcribeSyncLong("data/wav/cn/早饭吃西红柿炒鸡蛋.wav"); //ok
+        //transcribeSyncLong("data/wav/cn/早饭吃西红柿炒鸡蛋.wav"); //ok
         //transcribeSyncLong("/Users/liuwen/Downloads/temp/big/丧心病狂的谷歌语音识别-3分55秒.pcm"); //ID_ARGUMENT: Inline audio exceeds duration limit. Please use a GCS URI.
+        transcribeSyncLong("/Users/liuwen/Downloads/temp/1/早饭吃西红柿炒鸡蛋.pcm");
         logger.info("结束");
 
     }
@@ -40,19 +39,24 @@ public class RunCN {
      * @param fileName the path to a PCM audio file to transcribe.
      */
     public static void transcribeSyncLong(String fileName) throws Exception {
-        try (com.google.cloud.speech.v1.SpeechClient speech = com.google.cloud.speech.v1.SpeechClient.create()) {
+        try (SpeechClient speech = SpeechClient.create()) {
             Path path = Paths.get(fileName);
             byte[] data = Files.readAllBytes(path);
             ByteString audioBytes = ByteString.copyFrom(data);
 
+
+            ArrayList<String> languageList = new ArrayList<>();
+            languageList.add("en-US");//英语（美国）
+
             // Configure request with local raw PCM audio
-            com.google.cloud.speech.v1.RecognitionConfig config =
-                    com.google.cloud.speech.v1.RecognitionConfig.newBuilder()
-                             .setEncoding(com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding.LINEAR16)
+            RecognitionConfig config =
+                    RecognitionConfig.newBuilder()
+                             .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
                             .setLanguageCode("cmn-Hans-CN")
+                            .addAllAlternativeLanguageCodes(languageList)
                             .setSampleRateHertz(16000)
                             .build();
-            com.google.cloud.speech.v1.RecognitionAudio audio = com.google.cloud.speech.v1.RecognitionAudio.newBuilder().setContent(audioBytes).build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(audioBytes).build();
 
             // Use non-blocking call for getting file transcription
             OperationFuture<LongRunningRecognizeResponse, LongRunningRecognizeMetadata> response =
